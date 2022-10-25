cd6a421b4c76c2a3ae0a0b25ae7ad87277117cc3
Muhammad Alif Rahmat Novian Arsianto <kingboard154@gmail.com>
2022-10-25 14:04:47 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}', 'index.html'],
+  content: ["./public/**/*.{html,js}", "index.html"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary : '#14b8a6',
-        secondary: '#64748b',
-        dark : '#0f172a'
+        primary: "#ea580c",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screens: {
-        '2xl' : '1320px',
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
+};
